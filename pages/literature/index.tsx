7c0a2e00750aee4eacfe4e6593c6f3c215baa8ba
James McGahn <jmcgahn@gmail.com>
2023-09-19 13:51:24 -0400

    update campaign lit

--- pages/literature/index.tsx
+++ pages/literature/index.tsx
@@ -1,34 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
-import ImageModal from '../../components/ui/ImageModal';
-import Layout from '../../components/layout/Layout';
-import styles from '../../styles/Literature.module.css';
+import React from "react";
+import Head from "next/head";
+import ImageModal from "../../components/ui/ImageModal";
+import Layout from "../../components/layout/Layout";
+import styles from "../../styles/Literature.module.css";
 
-import micheleBio from '../../public/static/imgs/2022camplit/michele-bio.jpeg';
-import mikeBio from '../../public/static/imgs/2022camplit/mike-bio.jpeg';
-import familyMailer1 from '../../public/static/imgs/2022camplit/family-mailing-1.jpeg';
-import familyMailer2 from '../../public/static/imgs/2022camplit/family-mailing-2.jpeg';
+import electionBios from "../../public/static/imgs/2023camplit/WR-2023-Election-Bios.jpg";
+import electionTeam from "../../public/static/imgs/2023camplit/WR-2023-Election-Team.jpg";
 
 const Literature = () => {
   const imagesArry = [
     {
-      img: micheleBio,
-      alt: 'Micheles Bio Literature',
+      img: electionBios,
+      alt: "2023 Re-Elect Team Sarlo Bios",
       priority: true,
     },
     {
-      img: mikeBio,
-      alt: 'Mike Bio Literature',
-      priority: false,
-    },
-    {
-      img: familyMailer1,
-      alt: 'Mike and Michelles Family Literature',
-      priority: false,
-    },
-    {
-      img: familyMailer2,
-      alt: 'Mike and Michelles Family Literature (Continued)',
+      img: electionTeam,
+      alt: "2023 Re-Elect Team Sarlo",
       priority: false,
     },
   ];
@@ -38,7 +26,10 @@ const Literature = () => {
       <div className={styles.container}>
         <Head>
           <meta name="robots" content="all" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <title>Literature</title>
           <meta name="description" content="Wood-Ridge Democrats" />
           <link rel="icon" href="/favicon.ico" />
@@ -46,7 +37,10 @@ const Literature = () => {
           <meta property="og:url" content="https://www.wrdems.com/" />
           <meta property="og:title" content="Wood-Ridge Democrats" />
           <meta property="og:description" content="Elect The Sarlo Team" />
-          <meta property="og:image" content="https://www.wrdems.com/static/imgs/sarloteam.jpeg" />
+          <meta
+            property="og:image"
+            content="https://www.wrdems.com/static/imgs/sarloteam.jpeg"
+          />
         </Head>
         <main className={styles.main}>
           {imagesArry.map((img) => (
